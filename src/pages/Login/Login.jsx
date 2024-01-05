3e0e60172c3bff352fce84280bc745fdf9bebf22
nabin <aanabin18@gmail.com>
2024-01-05 20:16:55 +0600

    login autha added

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h1 className="text-3xl text-center">This is login Page</h1>
+        </div>
+    );
+};
+
+export default Login;
